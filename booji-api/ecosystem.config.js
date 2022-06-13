e7e1aa5ed18954edbbda4cb4e29d082e99eefa91
xiet <xiet@133.cn>
2022-06-13 17:52:27 +0800

    chore: 配置

--- booji-api/ecosystem.config.js
+++ booji-api/ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: "booji",
-      script: "./dist/main.js",
+      // script: "./dist/main.js",
+      script: "./index.js",
       instances: "max",
       exec_mode: "cluster",
       env: {
