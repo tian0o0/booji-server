d09bcc74af063555a66d7063f6ce519355619f25
Titan~ <xietian19941007@gmail.com>
2022-06-15 16:32:04 +0800

    Revert "chore: 配置"
    This reverts commit daf570c478e29f7bd60532e3ce8170c013212d0f.

--- booji-admin/src/config/constant.ts
+++ booji-admin/src/config/constant.ts
@@ -1,3 +1,27 @@
+// 基本前缀
+export const BASE_URL = {
+  development: "/api",
+  production: "/",
+  beta: "/beta",
+  release: "/release",
+};
+// 基本路径
+export const VITE_BASE_PATH = "/";
+// 请求接口地址TODO
+export const VITE_HTTP_URL = "http://43.154.59.117:3333/api";
+// export const VITE_HTTP_URL = "http://localhost:3333/api";
+// 应用名称
 export const VITE_APP_TITLE = "Booji Admin";
+// LOGO
 export const VITE_APP_LOGO =
   "http://raw.githubusercontent.com/tian0o0/pic/master/icon.png";
+// 开启包依赖分析 可视化
+export const VITE_APP_ANALYZE: boolean = true;
+// 开启Gzip压缩
+export const VITE_APP_COMPRESS_GZIP: boolean = true;
+// 开启Gzip压缩，删除原文件
+export const VITE_APP_COMPRESS_GZIP_DELETE_FILE: boolean = true;
+// 去除 console
+export const VITE_DROP_CONSOLE: boolean = true;
+// 开启兼容
+export const VITE_APP_LEGACY: boolean = true;
